c66da4ab4703d983116d0e03f990ce188179f808
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-01-24 23:26:45 +0700

    Create out.java

---
+++ demo/src/demo3/out.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package demo3;
+
+/**
+ * 
+ */
+public class out {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hoTen = "HoaiThuy";
+		int tuoi = 20;
+		System.out.print("Hoai Thuy nam nay 20 tuoi.");
+		System.out.println("Hoai Thuy nam nay 20 tuoi.");
+		System.out.printf("%s nam nay %d tuoi.", hoTen,tuoi);
+		
+	}
+
+}
