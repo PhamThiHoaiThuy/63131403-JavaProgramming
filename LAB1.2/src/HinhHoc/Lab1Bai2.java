a362f8f5d28e30dcbcc54aab3bd7e09d2c315edc
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-01-24 21:06:36 +0700

    lab1.2

---
+++ LAB1.2/src/HinhHoc/Lab1Bai2.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package HinhHoc;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Lab1Bai2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double dai, rong;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhap chieu dai: ");
+		dai = scanner.nextDouble();
+		System.out.print("Nhap chieu rong: ");
+		rong = scanner.nextDouble();
+		
+		double chuVi, dienTich;
+		chuVi = (dai+rong)*2;
+		dienTich = dai*rong;
+		System.out.printf("Chu vi cua HCN: %f", chuVi);
+		System.out.printf("Dien tich cua HCN: %f", dienTich);
+		System.out.printf("Canh nho nhat cua HCN: %f", Math.min(dai, rong));
+
+
+	}
+
+}
