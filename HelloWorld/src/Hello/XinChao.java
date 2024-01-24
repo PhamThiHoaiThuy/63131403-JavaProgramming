c4d2d9e182455e88750fdb3d481bc9738adb5f24
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-01-24 20:53:51 +0700

    Update XinChao.java

--- HelloWorld/src/Hello/XinChao.java
+++ HelloWorld/src/Hello/XinChao.java
@@ -14,6 +14,8 @@ public class XinChao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Xin chao toi la sinh vien K63");
+		System.out.println("Toi them mot dong moi");
+
 	}
 
 }
