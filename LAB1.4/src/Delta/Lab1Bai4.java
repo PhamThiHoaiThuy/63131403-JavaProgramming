174d4f4f04d8b94829627601ba6e9f0ae0d66859
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-01-24 21:31:50 +0700

    lab1.4

---
+++ LAB1.4/src/Delta/Lab1Bai4.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Delta;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Lab1Bai4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a,b,c;
+		double delta;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhap he so a: ");
+		a = scanner.nextDouble();
+		System.out.print("Nhap he so b: ");
+		b = scanner.nextDouble();
+		System.out.print("Nhap he so c: ");
+		c = scanner.nextDouble();
+		
+		delta = b*b -(4*a*c);
+		System.out.printf("Can delta la: %.2f", Math.sqrt(delta));
+	}
+
+}
