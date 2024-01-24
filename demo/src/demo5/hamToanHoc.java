346b66df4ea35df06d8e779946413da757451e9e
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-01-25 00:21:38 +0700

    Create hamToanHoc.java

---
+++ demo/src/demo5/hamToanHoc.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package demo5;
+
+/**
+ * 
+ */
+public class hamToanHoc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double a,b,x;
+		x = Math.random();
+		/*(Math.random()) * ((max - min) + 1)) + min*/
+		System.out.printf("Giá trị x ngẫu nhiên là: %d",(int)(x*((12-5)+1)+5 ));
+	}
+
+}
