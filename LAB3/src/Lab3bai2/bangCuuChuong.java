b10143340b78b9b437a882f8c667bfe615a1ade4
PhamThiHoaiThuy <thuy.pth.63cntt@ntu.edu.vn>
2024-02-21 23:02:50 +0700

    Update bangCuuChuong.java

--- LAB3/src/Lab3bai2/bangCuuChuong.java
+++ LAB3/src/Lab3bai2/bangCuuChuong.java
@@ -6,14 +6,19 @@ public class bangCuuChuong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		int x;
+		/*int x;
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Nhập bảng cửu chương cần tính");
 		x = scanner.nextInt();
 		for(int i= 1; i<=10; i++) {
 			System.out.printf("%d * %d = %d", x,i,x*i);
 			System.out.println();
+		}*/
+		for(int i=1; i<=9; i++) {
+			for(int j=1; j<=10; j++) {
+				System.out.printf("%d * %d = %d", i, j, i*j);
+				System.out.println();
+			}
 		}
 	}
 
